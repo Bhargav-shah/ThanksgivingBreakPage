2b75a75cb5a38f56db4e1b914ed0053a9a7a4492
rlinares1 <44908875+rlinares1@users.noreply.github.com>
2019-12-06 13:34:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var i = document.getElementById("irish");
+var t = document.getElementById("thanksgiving");
+var c = document.getElementById("chickfila");
+
+function changeIrish(elem) {
+	i.classList.add("img1");
+}
+
+function changeThanksgiving(elem) {
+	t.classList.add("img2");
+}
+
+function changeChicken(elem) {
+	c.classList.add("img3");
+}
